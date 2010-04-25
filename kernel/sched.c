ce284ebb2949389339f87fe98044285ccc56f7c2
Tejun Heo <tj@kernel.org>
2010-04-25 09:41:42 +0200

    sched: consult online mask instead of active in select_fallback_rq()
    If called after sched_class chooses a CPU which isn't in a task's
    cpus_allowed mask, select_fallback_rq() can end up migrating a task
    which is bound to a !active but online cpu to an active cpu.  This is
    dangerous because active is cleared before CPU_DOWN_PREPARE is called
    and subsystems expect affinities of kthreads and other tasks to be
    maintained till their CPU_DOWN_PREPARE callbacks are complete.
    
    Consult cpu_online_mask instead.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ingo Molnar <mingo@elte.hu>

--- kernel/sched.c
+++ kernel/sched.c
@@ -2262,12 +2262,12 @@ static int select_fallback_rq(int cpu, struct task_struct *p)
 	const struct cpumask *nodemask = cpumask_of_node(cpu_to_node(cpu));
 
 	/* Look for allowed, online CPU in same node. */
-	for_each_cpu_and(dest_cpu, nodemask, cpu_active_mask)
+	for_each_cpu_and(dest_cpu, nodemask, cpu_online_mask)
 		if (cpumask_test_cpu(dest_cpu, &p->cpus_allowed))
 			return dest_cpu;
 
 	/* Any allowed, online CPU? */
-	dest_cpu = cpumask_any_and(&p->cpus_allowed, cpu_active_mask);
+	dest_cpu = cpumask_any_and(&p->cpus_allowed, cpu_online_mask);
 	if (dest_cpu < nr_cpu_ids)
 		return dest_cpu;
 
