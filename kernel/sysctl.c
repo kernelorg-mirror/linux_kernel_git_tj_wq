bd9aed46cde228b6371f80c2c65cf702c7bc3d10
Tejun Heo <tj@kernel.org>
2010-04-25 09:41:48 +0200

    slow-work: kill it
    slow-work doesn't have any user left.  Kill it.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: David Howells <dhowells@redhat.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -49,7 +49,6 @@
 #include <linux/acpi.h>
 #include <linux/reboot.h>
 #include <linux/ftrace.h>
-#include <linux/slow-work.h>
 #include <linux/perf_event.h>
 #include <linux/kprobes.h>
 
@@ -876,13 +875,6 @@ static struct ctl_table kern_table[] = {
 		.maxlen         = sizeof(int),
 		.mode           = 0644,
 		.proc_handler	= proc_dointvec,
-	},
-#endif
-#ifdef CONFIG_SLOW_WORK
-	{
-		.procname	= "slow-work",
-		.mode		= 0555,
-		.child		= slow_work_sysctls,
 	},
 #endif
 #ifdef CONFIG_PERF_EVENTS
