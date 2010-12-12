4118fd1da8ce21f4c86fd3d09ccc7dc3a2da37ce
Tejun Heo <tj@kernel.org>
2010-12-12 19:55:49 +0100

    dm: use non-reentrant workqueues if equivalent
    kmirrord_wq, kcopyd_work and md->wq are created per dm instance and
    serves only single work item from the dm instance, so non-reentrant
    workqueues would provide the same ordering guarantees as ordered ones
    while allowing CPU affinity and use of the workqueues for other
    purposes.  Switch them to non-reentrant workqueues.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Mike Snitzer <snitzer@redhat.com>
    Cc: dm-devel@redhat.com

--- drivers/md/dm-raid1.c
+++ drivers/md/dm-raid1.c
@@ -1077,7 +1077,8 @@ static int mirror_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 	ti->split_io = dm_rh_get_region_size(ms->rh);
 	ti->num_flush_requests = 1;
 
-	ms->kmirrord_wq = alloc_ordered_workqueue("kmirrord", WQ_MEM_RECLAIM);
+	ms->kmirrord_wq = alloc_workqueue("kmirrord",
+					  WQ_NON_REENTRANT | WQ_MEM_RECLAIM, 0);
 	if (!ms->kmirrord_wq) {
 		DMERR("couldn't start kmirrord");
 		r = -ENOMEM;
