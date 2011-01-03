f08e00436a9628954a1a4c0562ec051262dac1ea
Tejun Heo <tj@kernel.org>
2011-01-03 12:20:36 +0100

    dlm: dlm workqueues aren't used in memory reclaim path
    dlm isn't depended upon during memory reclaim and so are its two
    workqueues.  Convert to alloc[_ordered]_workqueue() without
    WQ_MEM_RECLAIM.  While at it, fix workqueue allocation failure
    handling.  They return %NULL on failure not ERR_PTR() value.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Christine Caulfield <ccaulfie@redhat.com>
    Cc: David Teigland <teigland@redhat.com>
    Cc: cluster-devel@redhat.com

--- fs/dlm/lowcomms.c
+++ fs/dlm/lowcomms.c
@@ -1430,20 +1430,17 @@ static void work_stop(void)
 
 static int work_start(void)
 {
-	int error;
-	recv_workqueue = create_workqueue("dlm_recv");
-	error = IS_ERR(recv_workqueue);
-	if (error) {
-		log_print("can't start dlm_recv %d", error);
-		return error;
+	recv_workqueue = alloc_workqueue("dlm_recv", 0, 1);
+	if (!recv_workqueue) {
+		log_print("can't start dlm_recv");
+		return -ENOMEM;
 	}
 
-	send_workqueue = create_singlethread_workqueue("dlm_send");
-	error = IS_ERR(send_workqueue);
-	if (error) {
-		log_print("can't start dlm_send %d", error);
+	send_workqueue = alloc_ordered_workqueue("dlm_send", 0);
+	if (!send_workqueue) {
+		log_print("can't start dlm_send");
 		destroy_workqueue(recv_workqueue);
-		return error;
+		return -ENOMEM;
 	}
 
 	return 0;
