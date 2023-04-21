e4546aba2245a960f4270265577d3af9f6ae9b5b
Tejun Heo <tj@kernel.org>
2023-04-20 16:42:01 -1000

    cifs: Use alloc_ordered_workqueue() to create ordered workqueues
    BACKGROUND
    ==========
    
    When multiple work items are queued to a workqueue, their execution order
    doesn't match the queueing order. They may get executed in any order and
    simultaneously. When fully serialized execution - one by one in the queueing
    order - is needed, an ordered workqueue should be used which can be created
    with alloc_ordered_workqueue().
    
    However, alloc_ordered_workqueue() was a later addition. Before it, an
    ordered workqueue could be obtained by creating an UNBOUND workqueue with
    @max_active==1. This originally was an implementation side-effect which was
    broken by 4c16bd327c74 ("workqueue: restore WQ_UNBOUND/max_active==1 to be
    ordered"). Because there were users that depended on the ordered execution,
    5c0338c68706 ("workqueue: restore WQ_UNBOUND/max_active==1 to be ordered")
    made workqueue allocation path to implicitly promote UNBOUND workqueues w/
    @max_active==1 to ordered workqueues.
    
    While this has worked okay, overloading the UNBOUND allocation interface
    this way creates other issues. It's difficult to tell whether a given
    workqueue actually needs to be ordered and users that legitimately want a
    min concurrency level wq unexpectedly gets an ordered one instead. With
    planned UNBOUND workqueue updates to improve execution locality and more
    prevalence of chiplet designs which can benefit from such improvements, this
    isn't a state we wanna be in forever.
    
    This patch series audits all callsites that create an UNBOUND workqueue w/
    @max_active==1 and converts them to alloc_ordered_workqueue() as necessary.
    
    WHAT TO LOOK FOR
    ================
    
    The conversions are from
    
      alloc_workqueue(WQ_UNBOUND | flags, 1, args..)
    
    to 
    
      alloc_ordered_workqueue(flags, args...)
    
    which don't cause any functional changes. If you know that fully ordered
    execution is not ncessary, please let me know. I'll drop the conversion and
    instead add a comment noting the fact to reduce confusion while conversion
    is in progress.
    
    If you aren't fully sure, it's completely fine to let the conversion
    through. The behavior will stay exactly the same and we can always
    reconsider later.
    
    As there are follow-up workqueue core changes, I'd really appreciate if the
    patch can be routed through the workqueue tree w/ your acks. Thanks.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Steve French <sfrench@samba.org>
    Cc: Paulo Alcantara <pc@cjr.nz>
    Cc: Ronnie Sahlberg <lsahlber@redhat.com>
    Cc: Shyam Prasad N <sprasad@microsoft.com>
    Cc: Tom Talpey <tom@talpey.com>
    Cc: linux-cifs@vger.kernel.org
    Cc: samba-technical@lists.samba.org

--- fs/cifs/dfs_cache.c
+++ fs/cifs/dfs_cache.c
@@ -290,7 +290,7 @@ int dfs_cache_init(void)
 	int rc;
 	int i;
 
-	dfscache_wq = alloc_workqueue("cifs-dfscache", WQ_FREEZABLE | WQ_UNBOUND, 1);
+	dfscache_wq = alloc_ordered_workqueue("cifs-dfscache", WQ_FREEZABLE);
 	if (!dfscache_wq)
 		return -ENOMEM;
 
